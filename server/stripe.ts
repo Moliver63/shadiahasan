5660a481dfbf578bbbf8dba5d6e9fea4304cd243
Moliver63 <159635298+Moliver63@users.noreply.github.com>
2026-03-14 10:48:07 -0300

    Update Stripe API version in configuration

--- server/stripe.ts
+++ server/stripe.ts
@@ -9,7 +9,7 @@ import Stripe from "stripe";
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "";
 
 export const stripe = stripeSecretKey
-  ? new Stripe(stripeSecretKey, { apiVersion: "2026-01-28.clover" })
+   new Stripe(stripeSecretKey, { apiVersion: "2024-11-20.acacia" })
   : null;
 
 /**

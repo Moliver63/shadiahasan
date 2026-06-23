3965d97092b7896558865ca9b13552455ccf9d11
Moliver63 <159635298+Moliver63@users.noreply.github.com>
2026-06-23 18:48:01 -0300

    feat: register /admin/courses/:id/groups route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,6 +19,7 @@ import AdminPlans from "./pages/AdminPlans";
 import AdminTestimonials from "./pages/AdminTestimonials";
 import AdminCourseLessons from "./pages/AdminCourseLessons";
 import AdminLessonMaterials from "./pages/AdminLessonMaterials";
+import AdminCourseGroups from "./pages/AdminCourseGroups";
 import AdminSettings from "./pages/AdminSettings";
 import AdminLessons from "./pages/AdminLessons";
 import AdminStudents from "./pages/AdminStudents";
@@ -110,6 +111,7 @@ function Router() {
       <ProtectedRoute path={"/admin/settings"} component={AdminSettings} requireAdmin />
       <ProtectedRoute path={"/admin/lessons"} component={AdminLessons} requireAdmin />
       <ProtectedRoute path={"/admin/lessons/:id/materials"} component={AdminLessonMaterials} requireAdmin />
+      <ProtectedRoute path={"/admin/courses/:id/groups"} component={AdminCourseGroups} requireAdmin />
       <ProtectedRoute path={"/admin/students"} component={AdminStudents} requireAdmin />
       <Route path={"/ebooks"} component={Ebooks} />
       <ProtectedRoute path={"/ebook/:id"} component={EbookReader} />
